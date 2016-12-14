ffe88a8af4887981d2ed83dc306d2e148da7930c
Dave Hards <davehards@Daves-Mac-mini.local>
2016-12-13 16:12:38 -0800

    added more lines, cleanup

--- Assets/Scripts/ReduxGuiController.cs
+++ Assets/Scripts/ReduxGuiController.cs
@@ -7,51 +7,27 @@ using UnityEngine.EventSystems;
 public class ReduxGuiController : MonoBehaviour 
 {
 	public GameObject StartLineProxy;
-
 	public GameObject TopLineProxy;
-
 	public List <GameObject> ListTextLineObjects;
-
-	//private List <GameObject> TextLineObjects = null;
-
 	public static ReduxGuiController Instance;
-
 	private GameObject TextLinesContainer;
-	private const int objectPoolSize = 11;//22
-
 	private int CurrentLineIndex = 0;
 
 	void Awake () 
 	{
 		Instance = this;
 
-		//TextLineObjects = new List<GameObject>();
 	}
 
 	void Start () 
 	{
-		//TextLinesContainer = GameObject.Find ("FuelPanel");
-
 		RectTransform rt = (RectTransform)StartLineProxy.transform;
 		float dy = rt.rect.height;
 
-		for (int t = 0; t < objectPoolSize; t++) 
+		int objCount = ListTextLineObjects.Count;
+
+		for (int t = 0; t < objCount; t++) 
 		{
-			//GameObject _lineObj = Instantiate (Resources.Load ("TextLineObj", typeof(GameObject))) as GameObject;
-			//GameObject _lineObj = Instantiate (TopLineProxy);
-
-			//Debug.Log ("TextLinesContainer = " + TextLinesContainer);
-			//if (TextLinesContainer != null) {
-			//	_lineObj.transform.parent = TextLinesContainer.transform;
-			//}
-
-			//_lineObj.name = "LineObj" + t.ToString ();
-
-			//_lineObj.transform.position = new Vector3(StartLineProxy.transform.position.x, StartLineProxy.transform.position.y - ((float)t * dy), StartLineProxy.transform.position.z);
-
-			//_lineObj.gameObject.
-			//_lineObj.transform.localScale = new Vector3(2, 1, 1);
-
 			GameObject _lineObj = ListTextLineObjects [t];
 
 			TextWindowLine lineObjectScript = _lineObj.GetComponent<TextWindowLine> ();
@@ -61,12 +37,9 @@ public class ReduxGuiController : MonoBehaviour
 				lineObjectScript.lineIndex = t;
 			} else {
 				Debug.Log ("lineObjectScript == null");
-			}
-				
-			//TextLineObjects.Add (_lineObj);
+			}				
 		}
-
-
+			
 		QueryLineObjectsClear ();
 	}
 	
@@ -82,7 +55,6 @@ public class ReduxGuiController : MonoBehaviour
 			TextWindowLine lineObjectScript = tObj.GetComponent<TextWindowLine> ();
 			lineObjectScript.SetLineText (".");
 		}
-
 		CurrentLineIndex = 0;
 	}
 		
@@ -90,9 +62,7 @@ public class ReduxGuiController : MonoBehaviour
 	{
 		FuelManager.Instance.StartGetEventsCorroutine ();
 	}
-
-
-
+		
 	public void addTextToWindow (string text) 
 	{
 		QueryLineObjectsAddTextLine (text);
@@ -113,7 +83,5 @@ public class ReduxGuiController : MonoBehaviour
 			}
 		}
 	}
-
-
 
 }
